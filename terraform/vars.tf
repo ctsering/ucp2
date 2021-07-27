eb3a15c8871656d2e3c0a49b7c361b1ceef12d6d
acarrasco@softool.org <angel@3e9b17ecafd5>
2021-07-27 21:15:31 +0000

    actualizaciópn vía ansible

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,18 +1,18 @@
 variable "vm_size" {
   type = string
   description = "Tamaño de la máquina virtual"
-  default = "Standard_D1_v2" # 3.5 GB, 1 CPU 
+  default = "Standard_D1_v2" # 3.5 GB, 1 CPU
 }
 
 variable "envs" {
 	type = list(string)
 	description = "Entornos"
-	default = ["dev", "pre", "pro"]
+	default = ["pro", "pre", "dev"]
 }
 
 
 variable "vms" {
         type = list(string)
         description = "Maquinas"
-        default = ["master", "worker1", "nfs"]
+        default = ["master-nfs", "worker1", "worker2"]
 }
